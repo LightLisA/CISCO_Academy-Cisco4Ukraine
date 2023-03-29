bf0436209ff47448657376146c003b00cba6e6c9
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2023-03-29 22:44:14 +0300

    Added LB__3_4_1_15.py

--- LB__3_4_1_15.py
+++ LB__3_4_1_15.py
@@ -20,15 +20,15 @@ class Triangle:
         self.var3 = vertice3
 
     def perimeter(self):
-        self.var_1_2 = math.sqrt(math.pow(Point.getx(self.var2) - Point.getx(self.var1), 2) \
+        var_1_2 = math.sqrt(math.pow(Point.getx(self.var2) - Point.getx(self.var1), 2) \
                                  + math.pow(Point.gety(self.var2) - Point.gety(self.var1), 2))
-        self.var_1_3 = math.sqrt(math.pow(Point.getx(self.var3) - Point.getx(self.var1), 2) \
+        var_1_3 = math.sqrt(math.pow(Point.getx(self.var3) - Point.getx(self.var1), 2) \
                                  + math.pow(Point.gety(self.var3) - Point.gety(self.var1), 2))
-        self.var_2_3 = math.sqrt(math.pow(Point.getx(self.var3) - Point.getx(self.var2), 2) \
+        var_2_3 = math.sqrt(math.pow(Point.getx(self.var3) - Point.getx(self.var2), 2) \
                                  + math.pow(Point.gety(self.var3) - Point.gety(self.var2), 2))
 
-        self.Perim = self.var_1_2 + self.var_1_3 + self.var_2_3
-        return self.Perim
+        Perimeter = var_1_2 + var_1_3 + var_2_3
+        return Perimeter
 
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
