c5992671d5ebb04732ddea2f3c61584ca4439c01
olly1215 <nenormalnuy@ukr.net>
2023-05-02 12:20:50 +0300

    add comments

--- G__ 4_3_1_11__Working with real files_Bytearrays.py
+++ G__ 4_3_1_11__Working with real files_Bytearrays.py
@@ -1,5 +1,11 @@
 from os import strerror
 
+#Look at the code in the editor. Let's analyze it:
+#
+#    - first, we initialize bytearray with subsequent values starting from 10; if you want the file's contents to be clearly readable, replace 10 with something like ord('a') - this will produce bytes containing values corresponding to the alphabetical part of the ASCII code (don't think it will make the file a text file - it's still binary, as it was created with a wb flag);
+#    - then, we create the file using the open() function - the only difference compared to the previous variants is the open mode containing the b flag;
+#    - the write() method takes its argument (bytearray) and sends it (as a whole) to the file;
+#    - the stream is then closed in a routine way.
 data = bytearray(10)
 
 for i in range(len(data)):
@@ -14,6 +20,12 @@ except IOError as e:
 
 
 # Your code that reads bytes from the stream should go here.
+#Let's analyze it:
+#
+#    - first, we open the file (the one you created using the previous code) with the mode described as rb;
+#    - then, we read its contents into the byte array named data, of size ten bytes;
+#    - finally, we print the byte array contents - are they the same as you expected?
+
 data = bytearray(10)
 
 try:
@@ -27,6 +39,9 @@ except IOError as e:
     print("I/O error occurred:", strerror(e.errno))
 
 print("\n")
+
+
+# ################################################################
 
 try:
     bf = open('C:\\Users\\olly1215\\Desktop\\file.bin', 'rb')
