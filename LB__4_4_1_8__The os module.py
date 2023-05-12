9f47ccb7e0cfda2e4c576651d679396b65d90218
olly1215 <nenormalnuy@ukr.net>
2023-05-12 17:26:11 +0300

    update: -- 4.4.1.8

--- LB__4_4_1_8__The os module.py
+++ LB__4_4_1_8__The os module.py
@@ -30,28 +30,43 @@ Example output:
 import os
 
 
-class MakeTree:
-    def __init__(self):
-        self.parent_dir = "C:\\Users\\olly1215\\Desktop\\Geek"
-        self.new_path = ''
+def make_dir(directory):  # створюємо ієрархію із папок яку задали нижще
+    new_path = parent_dir
+    for i in directory:
+        new_path = os.path.join(new_path, i)
+        if os.path.exists(new_path):
+            # print("The Directory <'% s'> exists " % i)
+            continue
+        else:
+            os.mkdir(new_path)
+    print("** New Directory path <'% s'>   created " % new_path)
 
-    def make_dir(self, directory):
-        self.new_path = self.parent_dir
-        for i in directory:
-            self.new_path = os.path.join(self.new_path, i)
-            if os.path.exists(self.new_path):
-                continue
+
+def dir_list_folder(head_dir, dir_name):    # шукаємо потрібну папку через рукурсію
+    dirList = []
+    for fn in os.listdir(head_dir):     # список папок у Папці
+        dirfile = os.path.join(head_dir, fn)    # з'єднуємо парент шлях і папку
+        if os.path.isdir(dirfile):
+            if fn.upper() == dir_name.upper():  # порівнюємо поточний каталог із заданой папкой
+                dirList.append(dirfile)     # добавляємо в список
             else:
-                os.mkdir(self.new_path)
-        print("New Directory path <'% s'> was created " % self.new_path)
+                dirList += dir_list_folder(dirfile, dir_name)   # якщо небуло співпадінь ідемо шукаті далі
+    return dirList
 
 
-class SearchFolder:
-    pass
+def list_folder_for_remove(head_dir):   # удаляемо папки через рукурсію
+    for fn in os.listdir(head_dir):
+        dirfile = os.path.join(head_dir, fn)
+        list_folder_for_remove(dirfile)
+        if os.path.isdir(dirfile):
+            os.rmdir(dirfile)
+            print("***  <% s> removed successfully" % dirfile)
 
 
-class RemoveTree:
-    pass
+def remove_tree(header_dir):
+    print('\n')
+    os.chdir(header_dir)
+    list_folder_for_remove(header_dir)
 
 
 tree = 'tree'
@@ -59,9 +74,9 @@ c = 'c'
 cpp = 'cpp'
 python = 'python'
 other_courses = 'other_courses'
+parent_dir = "C:\\Users\\olly1215\\Desktop\\Geek"
 
-newPath = MakeTree()
-
+#  ієрархія папов
 path_1_1 = [tree, c, other_courses, cpp]
 path_1_2 = [tree, c, other_courses, python]
 path_2_1 = [tree, cpp, other_courses, c]
@@ -69,9 +84,24 @@ path_2_2 = [tree, cpp, other_courses, python]
 path_3_1 = [tree, python, other_courses, c]
 path_3_2 = [tree, python, other_courses, cpp]
 
+# створили список із шєрархії каталогів і посилаємо на створення
 dir_list = [path_1_1, path_1_2, path_2_1, path_2_2, path_3_1, path_3_2]
 for lis_dir in dir_list:
-    newPath.make_dir(lis_dir)
+    make_dir(lis_dir)
+print('\n')
 
-# path = input("Enter path, like: <./path> :")
-# directory = input("Enter directory, like: <dir> :")
+# path = input("Enter path, like: <C:\\Users\\Folder_1\\Folder_2> : ")
+# directory = input("Enter directory name, like: <python> or <c> or <cpp>: ")
+path = "C:\\Users\\olly1215\\Desktop\\Geek\\tree"
+directory = "python"
+
+#  передаємо на пошук потрібну нам папку
+list_of_directories = dir_list_folder(path, directory)
+if len(list_of_directories) == 0:
+    print("The <% s> directory is absent" % directory)
+else:
+    for item in list_of_directories:
+        print(item)
+
+# видаляемо всі каталоги які створили
+remove_tree(parent_dir)
